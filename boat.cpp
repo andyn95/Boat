f028a10c988d44b0b4ec458a2d524b39ea4a4534
root <root@DESKTOP-JGCMBVE.localdomain>
2020-04-10 18:55:59 -0700

    This is a program that prints out a boat, which was written in C++.

---
+++ boat.cpp
@@ -0,0 +1,37 @@
+// Andy Ninh
+// Boat program in C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "              ++" << endl;
+	cout << "             ++++" << endl;
+	cout << "             #   +" << endl;
+	cout << "           \"\"\"\"\"" << endl;
+	cout << "            /#" << endl;
+	cout << "           / #" << endl;
+	cout << "          /X #" << endl;
+	cout << "         /XX #" << endl;
+	cout << "        /XX  #'\\" << endl;
+	cout << "       /XXX  #''\\" << endl;
+	cout << "      /XXXX  #'''\\" << endl;
+	cout << "     /XXXXX  #''''\\" << endl;
+	cout << "    /XXXXXX  #'''''\\" << endl;
+	cout << "   /XXXXXXX  #''''''\\" << endl;
+	cout << "  /XXXXXXXX  #'''''''\\" << endl;
+	cout << " /           #        \\" << endl;
+	cout << "000000      ###     00000000" << endl;
+	cout << " ----------------------/" << endl;
+	cout << " \\||||||||||||||||||||/" << endl;
+	cout << "  \\||||||||||||||||||/" << endl;
+	cout << "   \\||||||||||||||||/" << endl;
+	cout << "    ================" << endl;
+	cout << "\n";
+	cout << "       USS America" << endl;
+	cout << "        Andy Ninh\n" << endl;
+}
+
+
+
